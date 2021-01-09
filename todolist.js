72a7783cb7a43fa1a85ec9f2fb7f625c5c10b9d4
GOYOOJIN <progolovego2020@gmail.com>
2021-01-09 21:53:59 +0900

    getTodos 05

--- todolist.js
+++ todolist.js
@@ -9,43 +9,45 @@ function dropTodo(){
   
 }
 
-function writeTodos(todos){
+function writeTodos(inputTodo){
   const li = document.createElement("li");
   const delBtn = document.createElement("button");
   const span = document.createElement("span");
   let Id = Date.now();  
   delBtn.innerText = "F";
   delBtn.addEventListener("click", dropTodo);
+  span.innerText = inputTodo;
   li.appendChild(span);
   li.appendChild(delBtn);
   li.id = Id;
   todolists.appendChild(li);
   const todoObj = {
-    text: text,
+    text: inputTodo,
     id: Id
   };
   todoarr.push(todoObj);
   saveTodos();
 }
 
-function saveTodos(todos){  
+function saveTodos(){  
   localStorage.setItem(TODOS, JSON.stringify(todoarr));  
 }
 
 function handleSubmit(event){
   event.preventDefault();
-  const todoinput = todoinput.value;
-  writeTodos(todoinput);
+  const inputTodo = todoinput.value;
+  console.log(inputTodo);
+  writeTodos(inputTodo);
   todoinput.value = "";
 }
 
 function loadTodos(){
   const loadTodos = localStorage.getItem(TODOS);
   if(loadTodos != null){
-    const jsonTodo = JSON.parse(loadTodos);
+    const jsonTodos = JSON.parse(loadTodos);
     jsonTodos.forEach(function(Todo){
-      writeTodos(Todo.text);
-    })
+      writeTodos(Todo.inputTodo);
+    });
   }
 }
 
